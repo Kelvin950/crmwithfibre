9aa965904dac9efb40bc1b35f0d83a8e26675682
kelvin <denlinato@gmail.com>
2023-10-15 08:20:31 +0000

    creating controllers

--- database/database.go
+++ database/database.go
@@ -1,4 +1,4 @@
-package main
+package database
 
 import (
 	"github.com/jinzhu/gorm"
@@ -10,3 +10,4 @@ var (
 
 	DBConn *gorm.DB
 )
+
--- main.go
+++ main.go
@@ -1,21 +1,36 @@
-package main 
+package main
 
-import {
+import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
-	"github.com/kelvin950/crmwithfibre/databse"
-}
+	"github.com/jinzhu/gorm"
+	"github.com/kelvin950/crmwithfibre/database"
+	"github.com/kelvin950/crmwithfibre/lead"
+)
 
 
 
  func setupRoutes(app *fiber.App){
 
-	app.Get(GetLeads)
-	app.Get(GetLead)
-	app.Post(NewLead)
-	app.Delete(DeleteLead)
+	app.Get("/api/v1/lead" ,lead.GetLeads)
+	app.Get("api/v1/lead/:id" , lead.GetLead)
+	app.Post("api/v1/lead" , lead.NewLead)
+	app.Delete("api/v1/lead/:id" ,lead.DeleteLead)
 }
 
 func initDb(){
+   var err error 
+
+   database.DBConn ,err =  gorm.Open("sqlite3" , "lead.db")
+   if err !=nil{
+	panic("Failed to connect ")
+} 
+
+fmt.Println("connection opened to database")
+database.DBConn.AutoMigrate(&lead.Lead{})
+fmt.Println("Database Migrated")
+
 
 }
 
@@ -23,8 +38,12 @@ func main(){
 
 	 app :=  fiber.New()
 
+	 initDb()
+	 
+	 defer database.DBConn.Close()
 	 setupRoutes(app)
 
 	 app.Listen(":3000")
+	 
     
 }
